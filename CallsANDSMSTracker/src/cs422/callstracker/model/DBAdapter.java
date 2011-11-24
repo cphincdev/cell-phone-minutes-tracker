25608c5be7489723b49cea7eac605d77758be8fc
sunnyboy.rohan@gmail.com <sunnyboy.rohan@gmail.com@e43bc879-8611-3e2c-e4e8-476aae9372ff>
2011-11-24 00:31:22 +0000

    Database methods

---
+++ CallsANDSMSTracker/src/cs422/callstracker/model/DBAdapter.java
@@ -0,0 +1,53 @@
+package cs422.callstracker.model;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DBAdapter {
+	private Context context;
+	DatabaseOpenHelper DB;
+	SQLiteDatabase db;
+	
+	public DBAdapter(Context context) {
+		this.context = context;
+	}
+	
+	protected SQLiteDatabase openConnection() {
+		DatabaseOpenHelper helper = new DatabaseOpenHelper(context);
+		return helper.getWritableDatabase();
+	}
+	
+	public boolean isEmpty(String table) {
+		db = openConnection();
+		Cursor cursor = db.query(table, new String[] {"COUNT(*)"}, null, null, null, null, null);
+		boolean result;
+		
+		cursor.moveToFirst();
+		if(cursor.getInt(0) == 0)
+			result = true;
+		else
+			result = false;
+		
+		cursor.close();
+		db.close();
+		return result;
+	}
+	
+	public long insert(String table, ContentValues values) {
+		db = openConnection();
+		db.delete(table, null, null);
+		long result = db.insert(table, null, values);
+		db.close();
+		return result;
+	}
+	
+	public Cursor getValues(String table) {
+		db = openConnection();
+		Cursor cursor = db.query(table, null, null, null, null, null, null);
+		cursor.moveToFirst();
+		db.close();
+		return cursor;
+	}
+}
