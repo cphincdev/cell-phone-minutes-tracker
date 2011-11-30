ad5cf288d872c35e1ee15205eec74d07ecd93026
sunnyboy.rohan@gmail.com <sunnyboy.rohan@gmail.com@e43bc879-8611-3e2c-e4e8-476aae9372ff>
2011-11-30 23:22:47 +0000

    30th Dec -  i. No Specific Changes  ii. following Single-Responsibility Principle  iii. following Open-Closeed Principle  iv. following Liskov Substitution Principle  v. following Dependency Inversion Principle  vi. following Interface Segregation Principle

--- CallsANDSMSTracker/src/cs422/callstracker/model/DatabaseOpenHelper.java
+++ CallsANDSMSTracker/src/cs422/callstracker/model/DatabaseOpenHelper.java
@@ -1,122 +1,60 @@
 package cs422.callstracker.model;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseOpenHelper extends SQLiteOpenHelper {
+public class DBAdapter {
+	private Context context;
+	DatabaseOpenHelper DB;
+	SQLiteDatabase db;
 	
-	public static final String DB_NAME = "CallsTrackerDB";
-	public static final int DB_VERSION = 1;
-	Cursor cur1,cur2;
-	public SQLiteDatabase db;
-	
-	public static final String TBL_STORAGE = "STORAGE_VALUE";
-	public static final String TBL_FAVORITE = "FAVORITE_NUMBER";
-
-	public String getPath(){
-		return db.getPath();
+	public DBAdapter(Context context) {
+		this.context = context;
 	}
 	
-	
-	public DatabaseOpenHelper(Context context) {
-		super(context, DB_NAME, null, DB_VERSION);
+	protected SQLiteDatabase openConnection() 
+	{
+		DatabaseOpenHelper helper = new DatabaseOpenHelper(context);
+		return helper.getWritableDatabase();
 	}
 	
-	public void createTables(SQLiteDatabase db) {
-		db.execSQL(
-			"CREATE TABLE " + TBL_STORAGE + " (" +
-				"_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-				"PHONE_NUM TEXT NOT NULL, " +
-				"SERVICE_PROVIDER TEXT, " +
-				"BILL_CYCLE_YEAR TEXT NOT NULL, " +
-				"BILL_CYCLE_MONTH TEXT NOT NULL, " +
-				"BILL_CYCLE_DAY TEXT NOT NULL, " +
-				"EVENING_START_HOUR TEXT, " +
-				"EVENING_START_MINUTE TEXT, " +
-				"EVENING_END_HOUR TEXT, " +
-				"EVENING_END_MINUTE TEXT, " +
-				"WEEKENDS_FREE TEXT, " +
-				"CALLER_ID TEXT NOT NULL, " +
-				"FREE_INCOMMING TEXT NOT NULL, " +
-				"PLAN_MINS TEXT NOT NULL, " +
-				"USED_MINS TEXT NOT NULL, " +
-				"SMS_PLAN TEXT NOT NULL, " +
-				"SMS_INCOMMING TEXT NOT NULL, " +
-				"TOTAL_SMS TEXT NOT NULL, " +
-				"USED_SMS TEXT NOT NULL," +
-				"CITY TEXT NOT NULL, " +
-				"FAV_FIVE TEXT NOT NULL, " +
-				"FAV_TEN TEXT NOT NULL" +
-			")"
-		);
+	public boolean isEmpty(String table) 
+	{
+		db = openConnection();
+		Cursor cursor = db.query(table, new String[] {"COUNT(*)"}, null, null, null, null, null);
+		boolean result;
 		
-		db.execSQL(
-			"CREATE TABLE " + TBL_FAVORITE + " (" +
-				"FAV_CHECK TEXT NOT NULL, " +
-				"FAV_NO TEXT NOT NULL, " +
-				"FAV_NUMBS_1 TEXT, " +
-				"FAV_NUMBS_2 TEXT, " +
-				"FAV_NUMBS_3 TEXT, " +
-				"FAV_NUMBS_4 TEXT, " +
-				"FAV_NUMBS_5 TEXT, " +
-				"FAV_NUMBS_6 TEXT, " +
-				"FAV_NUMBS_7 TEXT, " +
-				"FAV_NUMBS_8 TEXT, " +
-				"FAV_NUMBS_9 TEXT, " +
-				"FAV_NUMBS_10 TEXT" +
-			")"
-		);
-	}
-	
-	@SuppressWarnings({ "static-access", "unused" })
-	public boolean checkDBExists(){
-		SQLiteDatabase tempDB_tbl_storage=null;
-		SQLiteDatabase tempDB_tbl_favorite=null;
-		boolean flag=true;
-		try{
-			tempDB_tbl_storage=db.openDatabase(TBL_STORAGE, null, SQLiteDatabase.OPEN_READONLY);
-			tempDB_tbl_favorite=db.openDatabase(TBL_FAVORITE, null, SQLiteDatabase.OPEN_READONLY);
-			try{
-				cur1= tempDB_tbl_storage.rawQuery("SELECT * FROM "+TBL_STORAGE+";", null);
-				cur2= tempDB_tbl_favorite.rawQuery("SELECT * FROM "+TBL_FAVORITE+";", null);
-				if(cur1==null)
-					flag = false;
-				else
-					flag = true;
-					
-				
-			}
-			catch(SQLiteException e){
-				
-			}
-			
+		cursor.moveToFirst();
+		if(cursor.getInt(0) == 0)
+		{
+			result = true;
 		}
-		catch(SQLiteException e){
+		else
+		{
+			result = false;
 		}
-		cur1.close();
-		cur2.close();
-		return flag; 
+		cursor.close();
+		db.close();
+		return result;
 	}
 	
-	
-	
-	
-	public void dropTables(SQLiteDatabase db) {
-		db.execSQL("DROP TABLE " + TBL_STORAGE);
-		db.execSQL("DROP TABLE " + TBL_FAVORITE);
+	public long insert(String table, ContentValues values) 
+	{
+		db = openConnection();
+		db.delete(table, null, null);
+		long result = db.insert(table, null, values);
+		db.close();
+		return result;
 	}
 	
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		createTables(db);
-	}
-
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		dropTables(db);
-		createTables(db);
+	public Cursor getValues(String table) 
+	{
+		db = openConnection();
+		Cursor cursor = db.query(table, null, null, null, null, null, null);
+		cursor.moveToFirst();
+		db.close();
+		return cursor;
 	}
 }
